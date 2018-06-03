0ea77e9d0f0a53ec30fb529d2cfc29dded89cdcb
Allen Ussher <allen@ussher.ca>
2018-06-02 22:59:19 -0700

    Initial Commit

---
+++ Synthie/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Synthie
+//
+//  Created by Allen Ussher on 6/2/18.
+//  Copyright © 2018 Ussher Press. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Synthie/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Synthie
+//
+//  Created by Allen Ussher on 6/2/18.
+//  Copyright © 2018 Ussher Press. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
